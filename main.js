51c6a876d61258d5b27fe03ef61c93c9e5e62f3b
Tony Duran <xotony@yahoo.com>
2020-03-17 05:07:23 +0000

    Rearranged elements on screen.

--- main.js
+++ main.js
@@ -36,21 +36,3 @@
 //Go back to MAIN GAME LOOP.
 
 
-/*======================================
-
-Code samples
-
-//Returns a random integer from 1 to 10
-Math.floor(Math.random() * 10) + 1;  
-
-//Position elements on the screen.
-document.getElementById("block"+i).style.left = X_OFFSET + BLOCK_POS_X[i] + 'px';
-document.getElementById("block"+i).style.top  = BLOCK_POS_Y[i] + 'px';
-
-Cool animations
-https://jsfiddle.net/macloo/g39k3h3e/
-http://cssdeck.com/labs/pure-css-click-animations
-
-Sounds
-https://stackoverflow.com/questions/29594023/getting-camera-like-shutter-flash-effect-on-button-click-with-html5-and-jquery
-=======================================*/
