54a13374604e00e9bef991c30c42d9056f63f341
JohnADuran01 <2020-john.duran@warwickschools.org>
2020-03-24 23:49:51 +0000

    Hooked up three skill buttons

--- main.js
+++ main.js
@@ -22,6 +22,31 @@ function defendButton()
 
 }
 
+//Skill 1 button has been pressed
+function skill1Button()
+{
+  msg(attackOrder[currentAttacker] + " casts skill 1!");
+
+  getNextAttacker()
+
+}
+
+//Skill 2 Button has been pressed
+function skill2Button()
+{
+  msg(attackOrder[currentAttacker] + "casts skill 2!");
+
+  getNextAttacker()
+}
+
+//Skill 3 Button has been pressed
+function skill3Button()
+{
+  msg(attackOrder[currentAttacker] + " casts skill 3!");
+
+  getNextAttacker()
+}
+
   //Are all heroes dead?
   //  if (gameOver == 1)
   //      lostGame();
